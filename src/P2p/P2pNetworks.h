aa18a5a9cfb3285b977abfac8a25b214e0a13e9e
Bandit6121 <36683011+Bandit6121@users.noreply.github.com>
2018-03-11 15:45:35 -0600

    gehcoin
    gehcoin

---
+++ src/P2p/P2pNetworks.h
@@ -0,0 +1,15 @@
+// Copyright (c) 2011-2017 The Cryptonote developers
+// Copyright (c) 2014-2017 XDN developers
+// Copyright (c) 2016-2017 BXC developers
+// Copyright (c) 2017 Royalties developers
+// Copyright (c) 2010-2017 Kohaku developers
+// Copyright (c) 2017 Wayang developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#pragma once
+
+namespace CryptoNote
+{
+  const static boost::uuids::uuid CRYPTONOTE_NETWORK = { { 0x4a, 0x78, 0x0f, 0x0b, 0x44, 0xa0, 0x62, 0x93, 0xc7, 0x53, 0x8d, 0x8e, 0x10, 0x11, 0x01, 0x10 } };
+}
